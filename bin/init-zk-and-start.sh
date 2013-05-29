e6b54c02554d7b00263f3e0317564e5c7144abca
zhaigy1 <zhaigy@ucweb.com>
2013-05-29 19:45:09 +0800

    fix zk bug

--- bin/init-zk-and-start.sh
+++ bin/init-zk-and-start.sh
@@ -9,7 +9,7 @@ i=1
 for node in $ZK_NODES
 do
 echo $node
-ssh -p $SSH_PORT $node "mkdir -p ~/zookeeper_data/logs;echo '$i' > ~/zookeeper_data/myid"
+ssh -p $SSH_PORT $node "mkdir -p ~/zookeeper/datalogs ~/zookeeper/data;echo '$i' > ~/zookeeper_data/myid"
 #ssh -p $SSH_PORT $node sh ~/zookeeper/bin/zkServer-initialize.sh --myid=$i --force
 #ssh -p $SSH_PORT $node mkdir -p ~/zookeeper_data/logs
 ssh -p $SSH_PORT $node sh ~/zookeeper/bin/zkServer.sh start
