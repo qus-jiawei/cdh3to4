cde5321039bd9500aae008a9050fe2fa949aa46f
zhaigy1 <zhaigy@ucweb.com>
2013-05-31 09:59:28 +0800

    fix source bug

--- bin/5_hbase.sh
+++ bin/5_hbase.sh
@@ -3,7 +3,7 @@ UP_BIN=$(cd $(dirname $0);pwd)
 . $UP_BIN/head.sh
 var_die UP_ROOT
 
-ssh -p ${SSH_PORT} $START_HMASTER "$HBASE_BIN/start-hbase.sh;"
+ssh -p ${SSH_PORT} $START_HMASTER ". ~/.bash_profile;. $HBASE_BIN/start-hbase.sh;"
 sh $UP_BIN/start_close_check.sh hbase start
 sh $UP_BIN/hbase_check.sh
 
